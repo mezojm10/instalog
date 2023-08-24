8a2412e5afe7ec4067133d176c6a72298d436525
Mazin Abdallah <mezodrdr@gmail.com>
2023-08-24 21:10:30 +0300

    Add cors and use hosted url.

--- src/pages/api/events.ts
+++ src/pages/api/events.ts
@@ -1,9 +1,18 @@
+import NextCors from "nextjs-cors";
+
 import { createEvent } from "~/server/createEvent";
 import { queryEvents } from "~/server/listEvents";
 
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'POST'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   if (req.method === "GET") {
     await queryEvents(req, res);
     return;
--- src/useEvents.ts
+++ src/useEvents.ts
@@ -14,7 +14,7 @@ const getKey: SWRInfiniteKeyLoader = (pageIndex, previousPageData: Event[]) => {
 export function useEvents({ isLive, searchQuery }: { isLive: boolean, searchQuery: string }) {
     const { data, error, isLoading, size, setSize, mutate } = useSWRInfinite<Event[], Error>(getKey, async (key) => {
         const isSearch = searchQuery.trim().length > 0;
-        const resp = await fetch(`http://localhost:3000${key}` + (isSearch ? `search=${searchQuery}` : ""));
+        const resp = await fetch(`https://instalog-mezojm10.vercel.app${key}` + (isSearch ? `search=${searchQuery}` : ""));
         const data = eventFetchSchema.parse(await resp.json());
         if (!data.success) throw new Error(data.error);
         return data.events!;
